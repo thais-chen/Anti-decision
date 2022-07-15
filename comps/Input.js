78c812b8362dd0cb0377dfd675bf19adfa087f9f
thais-chen <onewithnature989@gmail.com>
2022-07-14 23:46:08 -0700

    added form

---
+++ comps/Input.js
@@ -0,0 +1,18 @@
+const Input = () => {
+    return (
+        <div>
+            <form>
+                <label>Option 1 </label>
+                <input
+                        type="text"
+                        placeholder="Ootoro"
+                        className="form--input"
+                        name="email"
+
+                    />
+                </form>
+        </div>
+      );
+}
+
+export default Input;
